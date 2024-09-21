c94915ed50fb5dc484e9edac30f4ed03be14ac85
lizwachira <lizwanguwachira@gmail.com>
2024-09-22 00:12:22 +0300

    feat:MongoDB connected

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const mongoose = require('monoose');
+const express = require('express')  //will run an express aplication
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
@@ -10,12 +10,29 @@ mongoose.connect('mongodb+srv://lizandrerw:lizandra2024@cluster0.beb3j.mongodb.n
 .then(() => console.log('MongoDB connected'))
 .catch((error) => console.log(error));
 
-const app = express()
-const PORT = PROCESS.env.PORT || 5000;
+const app = express()  //create an app
+const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
+        origin : 'http://localhost:5173', //every time i run my origin it runs on l/hst 5...
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            "Content-Type",
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+
+        ],
+        credentials: 'True' //will help in in loging in
 
         
     })
-)
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+
+app.listen(PORT, ()=> console.log(`Server is running on port $ {PORT}`)) //BASIC server config
